2041379feb66aea33f9d2098ca20d1c57f156ab1
535601882 <love_Fantasy@126.com>
2023-07-12 17:54:23 +0800

    refactor: lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,27 +2,27 @@
 "off"或者0:關閉規則 "warn"或1:開啟規則丟擲警告 "error"或2:開啟規則丟擲錯誤
 **/
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   // 定义规则类型
   rules: {
     // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat', // 新功能 feature
-        'fix', // 修复 bug
-        'docs', // 文档注释
-        'style', // 代码格式(不影响代码运行的变动)
-        'refactor', // 重构(既不增加新功能，也不是修复bug)
-        'perf', // 性能优化
-        'test', // 增加测试
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // 回退
-        'build' // 打包
-      ]
+        "feat", // 新功能 feature
+        "fix", // 修复 bug
+        "docs", // 文档注释
+        "style", // 代码格式(不影响代码运行的变动)
+        "refactor", // 重构(既不增加新功能，也不是修复bug)
+        "perf", // 性能优化
+        "test", // 增加测试
+        "chore", // 构建过程或辅助工具的变动
+        "revert", // 回退
+        "build", // 打包
+      ],
     ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never']
-  }
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+  },
 };
--- src/components/WaterMark/index.vue
+++ src/components/WaterMark/index.vue
@@ -3,6 +3,7 @@
 </template>
 <script>
 export default {
+  name: "WaterMark",
   props: {
     //显示的水印文本
     inputText: {
