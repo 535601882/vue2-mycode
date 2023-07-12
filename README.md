57f965cd997567559948d9de698192f3ed977135
chengjizhe <535601882@qq.com>
2023-07-12 16:45:35 +0800

    feat: commitlint

--- README.md
+++ README.md
@@ -112,16 +112,32 @@ hooks添加
 # commitlint 验证提交信息
 `yarn add  --dev @commitlint/cli @commitlint/config-conventional`
 
-提交格式
-<type>(<scope>): <subject>
+``` 
+   build：主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
+   ci：主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
+   docs：文档更新
+   feat：新增功能
+   merge：分支合并 Merge branch ? of ?
+   fix：bug 修复
+   perf：性能, 体验优化
+   refactor：重构代码(既没有新增功能，也没有修复 bug)
+   style：不影响程序逻辑的代码修改(修改空白字符，格式缩进，补全缺失的分号等，没有改变代码逻辑)
+   test：新增测试用例或是更新现有测试
+   revert：回滚某个更早之前的提交
+   chore：不属于以上类型的其他类型
+```
+   
+提交格式 
+
+`<type>(<scope>): <subject>`
 
 // 空一行
 
-<body>
+`<body>`
 
 各个标志意义即必要性：
 
-type（必需）scope（可选）subject（必需）body(可选)
+`type（必需）scope（可选）subject（必需）body(可选)`
 
  
 
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,3 @@
-/***
- * 说明：
-   commitlint.config.js 配置文件可以添加自己的规则，这里 @commitlint/config-conventional 提供了官方的规则扩展：
-   build：主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
-   ci：主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
-   docs：文档更新
-   feat：新增功能
-   merge：分支合并 Merge branch ? of ?
-   fix：bug 修复
-   perf：性能, 体验优化
-   refactor：重构代码(既没有新增功能，也没有修复 bug)
-   style：不影响程序逻辑的代码修改(修改空白字符，格式缩进，补全缺失的分号等，没有改变代码逻辑)
-   test：新增测试用例或是更新现有测试
-   revert：回滚某个更早之前的提交
-   chore：不属于以上类型的其他类型
- **/
 /**
 "off"或者0:關閉規則 "warn"或1:開啟規則丟擲警告 "error"或2:開啟規則丟擲錯誤
 **/
@@ -38,9 +22,7 @@ module.exports = {
         'build' // 打包
       ]
     ],
-    'subject-empty': [0],
-    // subject 大小写不做校验
-    'subject-case': [0],
-    'type-empty': [0]
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
   }
 };
