31e7160eb988d77edce28323f78af5e653483dd7
chengjizhe <535601882@qq.com>
2023-08-18 09:53:12 +0800

    feat: 增加err logs

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,7 @@
-import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
-export default function ({ i18n }) {
+export default function (Vue, { i18n }) {
   Vue.use(ElementUI, {
     size: "small",
     i18n: (key, value) => i18n.t(key, value),
--- src/plugins/error.js
+++ src/plugins/error.js
@@ -5,6 +5,20 @@ const { name } = require("/package");
 export default {
   // eslint-disable-next-line no-unused-vars
   install(Vue, options) {
+    class ResourceError extends Error {
+      constructor(message) {
+        super(message);
+        this.name = "ResourceError";
+      }
+    }
+
+    class Unhandledrejection extends Error {
+      constructor(message) {
+        super(message);
+        this.name = "Unhandledrejection";
+      }
+    }
+
     Vue.config.errorHandler = function (err, vm, info) {
       Vue.nextTick(() => {
         // // 添加 log
@@ -26,9 +40,46 @@ export default {
         }
       });
     };
+    // 可以捕获资源加载异常
+    window.addEventListener(
+      "error",
+      (event) => {
+        // 资源文件 加载错误处理
+        let tagName = event.target?.tagName;
+        if (["SCRIPT", "IMG"].includes(tagName)) {
+          console.log("资源错误", event);
+          store.dispatch("log/add", {
+            type: "error",
+            err: new ResourceError(`文件加载错误，${event.target.src}`),
+            vm: null,
+            info: `${event.target.src}`,
+          });
+          return;
+        }
+        console.log("文件错误", event);
+        // 添加 log
+        store.dispatch("log/add", {
+          type: "error",
+          err: event.error,
+          vm: null,
+          info: `错误信息: ${event.message},发生错误的脚本filename：${event.filename}， 发生错误的行号: ${event.lineno}, 发生错误的列号: ${event.colno}`,
+        });
+      },
+      true
+    );
 
-    window.onerror = function (e) {
-      console.log("错误====================", e);
-    };
+    window.addEventListener("unhandledrejection", (event) => {
+      // 在这里处理 Promise 错误，可以记录错误信息到日志服务
+      console.log(`UNHANDLED PROMISE REJECTION 错误：${event.reason}`, event);
+      // 添加 log
+      store.dispatch("log/add", {
+        type: "unhandledrejection",
+        err: new Unhandledrejection(event.reason),
+        vm: null,
+        info: event.reason.message,
+      });
+      // 阻止默认处理（例如将错误输出到控制台）
+      event.preventDefault();
+    });
   },
 };
