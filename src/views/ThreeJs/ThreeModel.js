0a1120efc61fdfa76c9e23a69ad2244293ebc9c4
程纪者 <535601882@qq.com>
2023-09-15 09:44:45 +0800

    feat: 利用threeJs开发射击游戏

--- src/views/ThreeJs/ThreeModel.js
+++ src/views/ThreeJs/ThreeModel.js
@@ -3,27 +3,27 @@ import * as THREE from "three";
 
 // 创建three
 class ThreeModel {
-  constructor(options){
-    this.width = options.width
-    this.height = options.height
-    this.parent = options.parent
-    this.helperStatus = options.helperStatus || false // 是否开启帮助
-    this.limitControls = options.limitControls || false// 限制角度
-    this.init()
+  constructor(options) {
+    this.width = options.width;
+    this.height = options.height;
+    this.parent = options.parent;
+    this.helperStatus = options.helperStatus || false; // 是否开启帮助
+    this.limitControls = options.limitControls || false; // 限制角度
+    this.init();
     if (this.helperStatus) {
-      this.initHelper()
+      this.initHelper();
     }
   }
   init() {
-    this.createScene()
-    this.createCamera()
-    this.createAmbientLight()
-    this.createRenderer()
-    this.createHelps()
+    this.createScene();
+    this.createCamera();
+    this.createAmbientLight();
+    this.createRenderer();
+    this.createHelps();
   }
   createHelps() {
-    this.controls = new OrbitControls(this.camera,this.renderer.domElement)
-    this.controls.update()
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.update();
     if (this.limitControls) {
       this.controls.minDistance = 1; // 最小缩放距离
       this.controls.maxDistance = 5; // 最大缩放距离
@@ -32,42 +32,42 @@ class ThreeModel {
     }
   }
   initHelper() {
-    this.sceneAdd( new THREE.AxesHelper( 50 ) );
-    this.sceneAdd( new THREE.CameraHelper( this.camera ) );
+    this.sceneAdd(new THREE.AxesHelper(50));
+    this.sceneAdd(new THREE.CameraHelper(this.camera));
   }
   // 创建场景
-  createScene(){
-    this.scene = new THREE.Scene()
+  createScene() {
+    this.scene = new THREE.Scene();
     // this.scene.background = new THREE.Color(0xAAAAAA); //清除并设置成略浅的灰色
   }
   // 创建相机
   createCamera() {
-    this.camera = new THREE.PerspectiveCamera(45,this.width/this.height,0.1,1000)
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 0.1, 1000);
     this.camera.position.set(0, 1, 5); // 设置摄像机位置在室内
     // this.camera.lookAt(0,0,0)
   }
   // 创建渲染器
   createRenderer() {
-    this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setSize(this.width,this.height)
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(this.width, this.height);
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.parent.appendChild(this.renderer.domElement);
   }
   // 添加物体到场景
   sceneAdd(model) {
-    this.scene.add(model)
-    return this
+    this.scene.add(model);
+    return this;
   }
   // 创建环境光
   createAmbientLight() {
-    this.sceneAdd(new THREE.AmbientLight(0xffffff ))
+    this.sceneAdd(new THREE.AmbientLight(0xffffff));
   }
   animate(cb) {
-    cb && cb()
-    requestAnimationFrame(() => this.animate(cb))
-    this.controls.update()
-    this.renderer.render(this.scene,this.camera)
+    cb && cb();
+    requestAnimationFrame(() => this.animate(cb));
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
-export default ThreeModel
+export default ThreeModel;
--- src/views/ThreeJs/components/ArchitectModel.vue
+++ src/views/ThreeJs/components/ArchitectModel.vue
@@ -1,52 +1,46 @@
 <template>
-  <div ref="loader">
-  </div>
+  <div ref="loader"></div>
 </template>
 
 <script>
-  import * as THREE from "three";
-  import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-  import ThreeModel from "../ThreeModel";
-  export default {
-    name: "ArchitectModel",
-    components: {},
-    data() {
-      return {}
-    },
-    computed: {},
-    created() {
-    },
-    methods: {},
-    mounted() {
-      this.threeModel = new ThreeModel({
-        width: 800,
-        height: 500,
-        parent: this.$refs.loader,
-        helperStatus: true
-      })
-      const loader = new GLTFLoader()
-      loader.load("/static/gltf/仓库大楼/scene.gltf",(gltf) => {
-        console.log("gltf",gltf)
-        let model = gltf.scene
-        model.position.set(0, -0.1, 0);
-        this.threeModel.sceneAdd(model)
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import ThreeModel from "../ThreeModel";
+export default {
+  name: "ArchitectModel",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  methods: {},
+  mounted() {
+    this.threeModel = new ThreeModel({
+      width: 800,
+      height: 500,
+      parent: this.$refs.loader,
+      helperStatus: true,
+    });
+    const loader = new GLTFLoader();
+    loader.load("/static/gltf/仓库大楼/scene.gltf", (gltf) => {
+      console.log("gltf", gltf);
+      let model = gltf.scene;
+      model.position.set(0, -0.1, 0);
+      this.threeModel.sceneAdd(model);
+    });
+    // 创建灯光
+    const light = new THREE.DirectionalLight(0xffffff);
+    this.threeModel.sceneAdd(light);
+    this.threeModel.sceneAdd(new THREE.DirectionalLightHelper(light, 5));
 
-      })
-      // 创建灯光
-      const light = new THREE.DirectionalLight( 0xffffff )
-      this.threeModel.sceneAdd(light)
-      this.threeModel.sceneAdd(new THREE.DirectionalLightHelper( light, 5 ))
-
-
-      // const geometry =  new THREE.BoxGeometry(1,1,1)
-      // const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-      // const cube =  new THREE.Mesh(geometry,material)
-      // this.threeModel.sceneAdd(cube)
-      this.threeModel.animate()
-    },
-  }
+    // const geometry =  new THREE.BoxGeometry(1,1,1)
+    // const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+    // const cube =  new THREE.Mesh(geometry,material)
+    // this.threeModel.sceneAdd(cube)
+    this.threeModel.animate();
+  },
+};
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
